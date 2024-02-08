99c9bebb234cf86e03db518b2fa7540df2ea4c52
Dilan Garcia <90223932+Dilan03@users.noreply.github.com>
2024-02-08 07:52:08 -0600

    Routing configured

---
+++ src/layouts/DrawingPanelLayout.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DrawingPanelLayout() {
+   return (
+      <div>Drawing Panel</div>
+   )
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,39 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
+import HomeLayout from "../layouts/HomeLayout";
+import DrawingPanelLayout from "../layouts/DrawingPanelLayout";
+import ProfileLayout from "../layouts/ProfileLayout";
+import SearchLayout from "../layouts/SearchLayout";
+import NotFound from "../pages/NotFound";
 
 export const router = createBrowserRouter([
    {
       path: "/",
       element: <Login />,
+   },
+   {
+      path: "/register",
+      element: <Register />,
+   },
+   {
+      path: "/home",
+      element: <HomeLayout />,
+   },
+   {
+      path: "/profile",
+      element: <ProfileLayout />,
+   },
+   {
+      path: "/drawingpanel",
+      element: <DrawingPanelLayout />,
+   },
+   {
+      path: "/search",
+      element: <SearchLayout />,
+   },
+   {
+      path: "*",
+      element: <NotFound />,
    },
 ]);
